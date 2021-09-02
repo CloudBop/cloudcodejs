72a70d9d010f900ca0505995475180284e181f3a
Cloudbop <boppincolin@gmail.com>
2021-09-02 04:46:38 +0100

    attempt to fix

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { createCellsRouter } from "./routes/cells";
+import path from "path";
 export const serve = (
   port: number,
   filename: string,
